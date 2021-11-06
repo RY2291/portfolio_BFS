d9efc1fddc301355d6eb14ac4cfe3b9b63e9ec34
RY2291 <prgr2498@gmail.com>
2021-11-06 12:07:32 +0000

    fix-all

---
+++ app/controllers/public/comments_controller.rb
@@ -0,0 +1,30 @@
+class Public::CommentsController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
+    if comment.save
+      flash[:notice] = "コメントが完了しました！"
+      redirect_to post_path(post.id)
+    else
+      flash[:notice] = "コメントに失敗しました"
+      redirect_back(fallback_location: top_path)
+    end
+  end
+
+  def destroy
+    Comment.find_by(id: params[:id]).destroy
+    flash[:notice] = "コメントを削除しました"
+    redirect_back(fallback_location: top_path)
+  end
+
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
+
+end
