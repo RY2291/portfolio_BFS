d1badb2144e5d81c29f9a9fcade21e8abdcc5718
RY2291 <prgr2498@gmail.com>
2021-11-06 05:16:30 +0000

    [add]投稿削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,50 @@
 class Public::PostsController < ApplicationController
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
       flash[:notice] = "投稿が完了しました！"
       redirect_to posts_path
-    else 
+    else
       flash[:notice] = "投稿に失敗しました"
       redirect_back(fallback_location: top_path)
     end
   end
-  
+
   def index
     @posts = Post.all
   end
-  
+
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
   end
-  
+
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to user_path(current_user)
   end
-  
+
   private
   def post_params
     #フォームが未完成のため、フォームを追加次第カラムを追加
     # [ ]は複数画像idのため必要
     params.require(:post).permit(:title, :introduction, :address, :post_image, post_images_images: [])
   end
-  
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  
+  belongs_to :user
+  
   has_many :post_images, dependent: :destroy
   accepts_attachments_for :post_images, attachment: :image
   attachment :post_image
