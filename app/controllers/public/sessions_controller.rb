46c0137636fe44c1c8a0cca474c2064c13ca383c
RY2291 <prgr2498@gmail.com>
2021-11-13 09:42:33 +0000

    [fix]地図表示, turbolinks

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -29,11 +29,12 @@ class Public::SessionsController < Devise::SessionsController
   def reject_user
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
-      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false)
+      if (@user.valid_password?(params[:user][:password])) && (@user.active_for_authentication? == false)
         flash[:alert] = "このアカウントは退会済みです。新しいアカウントを作成してください"
         redirect_to new_user_registration_path
       end
     else
       flash[:alert] = "必須項目を入力してください"
     end
+  end
 end
