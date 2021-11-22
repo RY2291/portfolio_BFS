d9da965f455d297412e002719cbfea8163d715b7
RY2291 <prgr2498@gmail.com>
2021-11-22 14:43:29 +0000

    [fix]フォローページレイアウト

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -11,10 +11,11 @@ before_action :authenticate_user!
     current_user.unfollow(params[:user_id], current_user.id)
     redirect_back(fallback_location: root_path)
   end
-  
+
   def follow
-    @following = User.find(params[:user_id]).followings.where.not(id: current_user.id)
+    @followings = User.find(params[:user_id]).followings.where.not(id: current_user.id)
     @followers =  User.find(params[:user_id]).followers.where.not(id: current_user.id)
+    @user = User.find(params[:user_id])
   end
 
 end
