0e4b6b976ab180da8acfdfacaadcc97f796105a0
RY2291 <prgr2498@gmail.com>
2021-11-24 06:49:44 +0000

    [add]RSpec FactoryBot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_14_020211) do
+ActiveRecord::Schema.define(version: 2021_11_08_113524) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -72,19 +72,6 @@ ActiveRecord::Schema.define(version: 2021_11_14_020211) do
   create_table "relationships", force: :cascade do |t|
     t.integer "following_id"
     t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tag_maps", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "tag_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
