3493da532a285ca8b796a421f04f1b33446ee6ea
RY2291 <prgr2498@gmail.com>
2021-11-25 09:43:37 +0000

    [add]RSpec Tagモデル

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,5 @@ class Tag < ApplicationRecord
   has_many :tag_maps, dependent: :destroy, foreign_key: "tag_id"
   has_many :posts, through: :tag_maps ,dependent: :destroy
   
+  validates :tag_name, length: { maximum: 10 }
 end
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe "Tagモデルのテスト" do
+  describe "バリデーションのテスト" do
+    
+    subject { tag.valid? }
+    
+    let(:tag_map) { create(:tag_map) }
+    let!(:post) { build(:post, user_id: user.id) }
+    
+    context "tag_nameカラム" do
+      it "10文字以下であること: 10文字はo" do
+        tag.tag_name = Faker::Lorem.chracters(10)
+        is_expected.to eq true
+      end
+      it "10文字以下であること: 11文字はx" do
+        ttag.tag_name = Faker::Lorem.chracters(11)
+        is_expected.to eq false
+      end
+    end
+  end
+end
