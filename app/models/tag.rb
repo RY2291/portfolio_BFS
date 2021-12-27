4860a353db9ae8b96bcf37d1eb8dee366e818eeb
RY2291 <prgr2498@gmail.com>
2021-12-27 09:10:19 +0000

    [fix]レスポンシブ

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ApplicationRecord
 
   has_many :tag_maps, dependent: :destroy, foreign_key: "tag_id"
   has_many :posts, through: :tag_maps ,dependent: :destroy
-  
+
   validates :tag_name, length: { maximum: 10 }
 end
